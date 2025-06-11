3af85d0a6bc25d1bfe95dfcaac4b84d51aa4c2fe
Licenia Alvear <licialvear@gmail.com>
2025-06-11 15:36:11 -0500

    feat: agrega rendeizado de las cartas en el DOM

--- src/js/card.js
+++ src/js/card.js
@@ -1,45 +1,51 @@
 const $cards = document.querySelector(".cards-container"),
-$template = document.getElementById("template-card").content,
-$fragment = document.createDocumentFragment(),
+  $template = document.getElementById("template-card").content,
+  $fragment = document.createDocumentFragment(),
+  cardContent = [
+    {
+      title: "Fresa",
+      img: "./public/fresa.png",
+    },
+    {
+      title: "Naranja",
+      img: "./public/naranja.png",
+    },
+    {
+      title: "Aguacate",
+      img: "./public/palta.png",
+    },
+    {
+      title: "Pera",
+      img: "./public/pera.png",
+    },
+    {
+      title: "Lechuga",
+      img: "./public/repollo.png",
+    },
+  ];
 
-cardContent = [
-{
-    title: "Fresa",
-    img: "./public/fresa.png"
-},
-{
-    title: "Naranja",
-    img: "./public/naranja.png"
-},
-{
-    title: "Aguacate",
-    img: "./public/palta.png"
-},
-{
-    title: "Pera",
-    img: "./public/pera.png"
-},
-{
-    title: "Lechuga",
-    img: "./public/repollo.png"
-}
-]
+const allCards = [...cardContent, ...cardContent];
 
-cardContent.forEach(el => {
-    $template.querySelector("img").setAttribute("src", el.img);
-    $template.querySelector("img").setAttribute("alt", el.title);
-    $template.querySelector("figcaption").textContent = el.title;
+allCards.sort(() => Math.random() - 0.5);
 
-    let $clone = document.importNode($template, true);
-    $fragment.appendChild($clone);
-})
+allCards.forEach((el) => {
+  let $clone = document.importNode($template, true);
+
+  $clone
+    .querySelector(".card-front img")
+    .setAttribute("src", "./public/img004.jpg");
+  $clone.querySelector(".card-front img").setAttribute("alt", "Reverso");
+
+  $clone.querySelector(".card-back img").setAttribute("src", el.img);
+  $clone.querySelector(".card-back img").setAttribute("alt", el.title);
+  $clone.querySelector("figcaption").textContent = el.title;
+
+  const cardInner = $clone.querySelector(".card-inner");
+  $clone.querySelector(".card").addEventListener("click", () => {
+    cardInner.classList.toggle("is-flipped");
+  });
+
+  $fragment.appendChild($clone);
+});
 
 $cards.appendChild($fragment);
-
-
-
-
-
-
-
-
