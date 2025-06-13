0d18d308f785b8575737a4accae27485478456e2
Licenia Alvear <licialvear@gmail.com>
2025-06-13 15:58:20 -0500

    feat(juego): agrega logica del cronometro

--- src/js/game.js
+++ src/js/game.js
@@ -5,9 +5,22 @@ export const mostrarCartas = () => {
     card.classList.add("is-flipped");
   });
 
-  setTimeout( () => {
+  setTimeout(() => {
     cards.forEach((card) => {
       card.classList.remove("is-flipped");
     });
   }, 5000);
 };
+
+const idTiempo = setInterval(temporizador, 1000);
+
+let contador = 50;
+function temporizador() {
+  let tiempo = document.getElementById("tiempo");
+  tiempo.innerHTML = `00:${contador.toString().padStart(2, "0")}`;
+  contador--;
+
+  if (contador < 0) {
+    clearInterval(idTiempo);
+  }
+}
