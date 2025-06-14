5344e1cc8c88fef58eeee2923a78c5bdf5c3aa01
Licenia Alvear <licialvear@gmail.com>
2025-06-14 15:41:13 -0500

    feat(juego): agrega logica de comparacion de cartas y sus transiciones

--- src/js/game.js
+++ src/js/game.js
@@ -24,3 +24,54 @@ function temporizador() {
     clearInterval(idTiempo);
   }
 }
+
+function controlarJuego() {
+  let primeraCarta = null;
+  let segundaCarta = null;
+
+  const contenedor = document.querySelector(".cards-container");
+
+  contenedor.addEventListener("click", (e) => {
+    const cardInner = e.target.closest(".card-inner");
+
+    if (
+      !cardInner ||
+      cardInner.classList.contains("is-flipped") ||
+      cardInner === primeraCarta
+    )
+      return;
+
+    cardInner.classList.add("is-flipped");
+
+    if (!primeraCarta) {
+      primeraCarta = cardInner;
+    } else {
+      segundaCarta = cardInner;
+
+      const img1 = primeraCarta
+        .querySelector(".card-back img")
+        .getAttribute("src");
+      const img2 = segundaCarta
+        .querySelector(".card-back img")
+        .getAttribute("src");
+
+      if (img1 === img2) {
+        primeraCarta = null;
+        segundaCarta = null;
+
+        console.log("Es un par");
+      } else {
+        console.log("No acertaste");
+
+        setTimeout(() => {
+          primeraCarta.classList.remove("is-flipped");
+          segundaCarta.classList.remove("is-flipped");
+          primeraCarta = null;
+          segundaCarta = null;
+        }, 1000);
+      }
+    }
+  });
+}
+
+controlarJuego();
