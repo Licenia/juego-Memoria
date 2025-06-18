f5af1fd6e7cc2b5efa14105fa21e67d6f8d41b91
Licenia Alvear <licialvear@gmail.com>
2025-06-18 15:13:07 -0500

    fix: ajustar rutas de imágenes para entorno de producción

--- src/js/card.js
+++ src/js/card.js
@@ -6,27 +6,27 @@ const $cards = document.querySelector(".cards-container"),
   cardContent = [
     {
       title: "Fresa",
-      img: "./public/fresa.png",
+      img: "/public/fresa.png",
     },
     {
       title: "Naranja",
-      img: "./public/naranja.png",
+      img: "/public/naranja.png",
     },
     {
       title: "Aguacate",
-      img: "./public/palta.png",
+      img: "/public/palta.png",
     },
     {
       title: "Pera",
-      img: "./public/pera.png",
+      img: "/public/pera.png",
     },
     {
       title: "Lechuga",
-      img: "./public/repollo.png",
+      img: "/public/repollo.png",
     },
     {
       title: "Platano",
-      img: "./public/platano.png",
+      img: "/public/platano.png",
     }
   ];
 
@@ -39,7 +39,7 @@ allCards.forEach((el) => {
 
   $clone
     .querySelector(".card-front img")
-    .setAttribute("src", "./public/gato.jpeg");
+    .setAttribute("src", "/public/gato.jpeg");
   $clone.querySelector(".card-front img").setAttribute("alt", "Reverso");
 
 
@@ -58,7 +58,7 @@ const $contenedorVidas = document.querySelector(".lives"),
 
 for (let i = 1; i < 4; i++) {
   let $clone2 = document.importNode($templateVidas, true);
-  $clone2.querySelector("img").setAttribute("src", "./public/corazon.png");
+  $clone2.querySelector("img").setAttribute("src", "/public/corazon.png");
   $clone2.querySelector("img").setAttribute("alt", "Corazon");
 
   $fragmentVidas.appendChild($clone2);
